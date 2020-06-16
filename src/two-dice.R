0e4c5397b4cff39e06868bb7058e413df02be1b4
alexhkurz <alexhkurz@gmail.com>
2020-06-16 16:27:41 -0700

    rolls instead of throws

--- src/two-dice.R
+++ src/two-dice.R
@@ -1,11 +1,11 @@
 library(tidyverse)
 
 # the number of times you want to roll the two dice
-number_of_throws <- 30 
+number_of_rolls <- 30 
 
 # sample two dice
-die1_sample <- as_tibble(rdunif(number_of_throws,1,6)) # all values from 1 to 6 are equally likely
-die2_sample <- as_tibble(rdunif(number_of_throws,1,6))
+die1_sample <- as_tibble(rdunif(number_of_rolls,1,6)) # all values from 1 to 6 are equally likely
+die2_sample <- as_tibble(rdunif(number_of_rolls,1,6))
 two_dice <- die1_sample + die2_sample # add the values of the two dice
 
 # plot
